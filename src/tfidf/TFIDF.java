76accf1543d97871394d8d1391b340d8e197d71d
jr8699 <jrebok2@gmail.com>
2017-05-18 10:20:10 -0400

    Added initial project files
    Yeah buddy

---
+++ src/tfidf/TFIDF.java
@@ -0,0 +1,23 @@
+package tfidf;
+
+/**
+ * TF-IDF Tool
+ * 
+ * This tool implements TF-IDF to rank words within a document set.
+ * This is to be coupled with a Deep Belief Network implementation to 
+ * classify documents.
+ * 
+ * @author Justin Rebok
+ *
+ */
+public class TFIDF {
+
+	/**
+	 * Main Method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
